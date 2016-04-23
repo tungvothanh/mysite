3b14b31c4688566384ba22079cb4126032a11b0e
tungvothanh <ttungvtca91@gmail.com>
2016-04-24 02:55:53 +0900

    add static file

--- mysite/settings.py
+++ mysite/settings.py
@@ -115,6 +115,11 @@ USE_L10N = True
 
 USE_TZ = True
 
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+
+# STATIC_URL = '/static/'
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
