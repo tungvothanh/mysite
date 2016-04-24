5c33af494055e68dd43f79f04f234c5a19428422
tungvothanh <ttungvtca91@gmail.com>
2016-04-24 02:04:02 +0000

    add css file

--- mysite/settings.py
+++ mysite/settings.py
@@ -115,13 +115,12 @@ USE_L10N = True
 
 USE_TZ = True
 
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
+# STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 # STATIC_URL = '/static/'
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
-
+STATIC_ROOT = "/home/vothanhtung/mysite/static"
 STATIC_URL = '/static/'
