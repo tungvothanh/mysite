d36b3dd1d6ca8c2d4adba6e6c35efa9c01fd6214
tungvothanh <ttungvtca91@gmail.com>
2016-05-30 18:18:13 +0900

    add account module

--- account/urls.py
+++ account/urls.py
@@ -4,9 +4,8 @@ from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
-    url(r'^login/$', auth_views.login, name='auth_views.login'),
-    url(r'^simple_login/$', views.simple_login, name='simple_login'),
+    # url(r'^$', views.index, name='index'),
+    url(r'^$', views.simple_login, name='simple_login'),
     url(r'^logout/$', views.logout, name='logout'),
     url(r'^change_password/$', views.change_password, name='change_password'),
 
